7aee7fb756e69e8edd3b002de7c6eda5d083e77a
GeekWx <geekwx@gmail.com>
2022-09-17 12:36:58 -0300

    finalizando notificacao

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,21 @@
-import { StatusBar } from 'react-native';
+import { StatusBar  } from 'react-native';
 import { useFonts,
 Inter_400Regular,
 Inter_600SemiBold,
 Inter_700Bold,
 Inter_900Black } from '@expo-google-fonts/inter';
 
-
+import * as Notifications from 'expo-notifications';
 
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 
+import './src/services/notificationConfigs';
 
+import { getPushNotificationToken} from './src/services/getPushNotificationToken';
+import { useEffect, useRef } from 'react';
+import { Subscription } from 'expo-modules-core';
 
 export default function App() {
 const [ fontsLoaded ] = useFonts({
@@ -20,6 +24,30 @@ const [ fontsLoaded ] = useFonts({
   Inter_700Bold,
   Inter_900Black 
 });
+
+
+const gethNotificationListener = useRef<Subscription>();
+const responseNotificationListener = useRef<Subscription>();
+
+
+useEffect(() => {
+  getPushNotificationToken();
+})
+useEffect(() => {
+  gethNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+                                              console.log(notification);
+                                            })
+  responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+    console.log(response);
+  })
+
+  return () => {
+    if(  gethNotificationListener.current && responseNotificationListener.current){
+      Notifications.removeNotificationSubscription(gethNotificationListener.current);
+      Notifications.removeNotificationSubscription(responseNotificationListener.current);
+    }
+  }
+}, [])
 
   return (
     <Background >
